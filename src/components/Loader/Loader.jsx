1827516e0e7c4eccb7e88103f26dc8243dbeacfc
Anton Kostiuk <antkos90@gmail.com>
2025-03-10 13:27:49 +0100

    fixed

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,20 @@
 import css from "../Loader/Loader.module.css";
-import { Blocks } from "react-loader-spinner";
+// import { Blocks } from "react-loader-spinner";
+import GridLoader from "react-spinners/GridLoader";
 
 export default function Loader({ loading }) {
   if (loading) {
     return (
-      <Blocks
-        height="80"
-        width="80"
-        color="#4fa94d"
-        ariaLabel="blocks-loading"
-        wrapperStyle={{}}
-        wrapperClass="blocks-wrapper"
-        visible={true}
-      />
+      <GridLoader className={css.loader} color="#4f1ed0" size={30} />
+      // <Blocks
+      //   height="80"
+      //   width="80"
+      //   color="#4fa94d"
+      //   ariaLabel="blocks-loading"
+      //   wrapperStyle={{}}
+      //   wrapperClass="blocks-wrapper"
+      //   visible={true}
+      // />
     );
   }
 }
